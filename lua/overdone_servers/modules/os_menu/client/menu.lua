2b395628c707285cb65cee2ca86292b4dbbe7c6e
RyanTheTechMan <ryan@rivergroup.com>
2023-05-16 00:06:53 -0400

    Added concommand for main menu and cleaned up bug

--- lua/overdone_servers/modules/os_menu/client/menu.lua
+++ lua/overdone_servers/modules/os_menu/client/menu.lua
@@ -1,13 +1,27 @@
-local OSMenu = OverdoneServers.Menu
-local module = OSMenu.Module
+local module = OverdoneServers.Modules.os_menu
+local OSMenu = module.Data
 
-if IsValid(OSMenu.Menu) then OSMenu.Menu:Remove() end
+local function CreateModuleTab(module)
+    local tab = vgui.Create("DButton", modulePanel)
+    tab:SetText("")
 
-local SizeX, SizeY = ScrW() * 0.75, ScrH() * 0.75
+    function tab:Paint(w,h)
+        draw.RoundedBox(0,0,0,w,h, Color(36,36,36,200))
+        draw.SimpleText(module.DisplayName, "DermaDefault", w/2, h/2, Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    end
 
-function OSMenu:BuildMenu()
+    function tab:DoClick()
+        Selected = module
+        print("Selected", Selected.DisplayName)
+    end
+
+    return tab
+end
+
+local function BuildMenu()
+    local SizeX, SizeY = ScrW() * 0.75, ScrH() * 0.75
+
     local panel = vgui.Create("Panel")
-    OSMenu.Menu = panel
 
     panel:MakePopup()
     panel:SetSize(SizeX, SizeY)
@@ -33,42 +47,19 @@ function OSMenu:BuildMenu()
         end
     end
 
-    local moduleAmount = #modules
-
-    local modulePanel = vgui.Create("Panel", panel)
+    local modulePanel = vgui.Create("DScrollPanel", panel)
     modulePanel:SetSize(SizeX * 0.2, 300)
     modulePanel:SetPos(100, SizeY * 0.5) -- TODO: make this correct position
 
-    local Selected = {}
+    local Selected = nil -- Format is module, or module.Settings.Category, or module.Settings.Category.SubCategory
 
-    function panel:ModuleTab(module, index)
-        local tab = vgui.Create("DButton", modulePanel)
-        tab:SetSize(SizeX * 0.1, SizeY * 0.1)
-        tab:SetText("")
+    for i,module in ipairs(modules) do -- TODO: or any other sorted function
+        local tab = CreateModuleTab(module)
+        -- tab:SetSize(SizeX * 0.1, SizeY * 0.1)
+        -- tab:SetPos(0, SizeY * 0.1 * (i-1))
 
-        function tab:Paint(w,h)
-            draw.RoundedBox(0,0,0,w,h, Color(36,36,36,200))
-            draw.SimpleText(OSMenu:GetModuleName(module), "DermaDefault", w/2, h/2, Color(255,255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-        end
-
-        function tab:DoClick()
-            Selected = module
-            print("Selected", Selected.DisplayName)
-        end
-
-        return tab
-    end
-
-    for i,module in ipairs(modules) do --TODO: or any other sorted function
-        local tab = panel:ModuleTab(module, i)
-        tab:SetSize(SizeX * 0.1, SizeY * 0.1)
-        tab:SetPos(0, SizeY * 0.1 * (i-1))
-        modulePanel:Add(tab)
+        modulePanel:AddItem(tab)
     end
 end
 
-function OSMenu:GetModuleName(module) -- TODO: Move this into module.DisplayName in library
-    return module.DisplayName or module.FolderName or "Invalid Module Name"
-end
-
-OSMenu:BuildMenu()
+concommand.Add("os_menu", BuildMenu)
